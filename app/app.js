4d9840e8b4b02ef1ff71085c9c1f87ab377f9a3f
Sheila Donnelly <sheila.donnelly@outlook.com>
2022-11-03 19:51:08 -0700

    JWT authentication completed

--- app/app.js
+++ app/app.js
@@ -22,6 +22,14 @@ import passport from 'passport';
 import passportLocal from 'passport-local';
 import flash from 'connect-flash';
 
+//modules for JWT support
+import cors from 'cors';   //allows cross site/domain requests - for api or server to have multiple connectiosn with different IPs - can expose you to hijacked cookies
+import passportJWT from 'passport-jwt';
+
+//define JWT aliases
+let JWTStrategy = passportJWT.Strategy;
+let ExtractJWT = passportJWT.ExtractJwt;    //library offering us to decrypt infomramation
+
 // Auth Step 2 - defien our auth strategy
 let localStrategy = passportLocal.Strategy;
 
@@ -38,6 +46,9 @@ import { MongoURI, Secret } from '../config/config.js';
 import indexRouter from './routes/index.route.server.js'
 import movieRouter from './routes/movies.route.server.js';
 import authRouter from './routes/auth.route.server.js';
+
+// import api Routes
+import authAPIRouter from './routes/api/auth-api.route.server.js';
 
 // Instantiate Express Application
 const app = express();
@@ -63,6 +74,7 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname,'/client')));
 app.use(express.static(path.join(__dirname,'../public')));
 
+app.use(cors());
 
 // Auth Step 4 - Setup Express Session
 app.use(session({
@@ -85,10 +97,36 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//setup JWT Options
+//where we will look for auth info - server to client & client to server - 
+//http message with:
+// 1) header(static info about client - an envelope) - checks token from http envelope
+// 2)content(info shared btwn server & client)
+let jwtOptions = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),       //to extract header token
+    secretOrKey: Secret     //to encrypt token
+}
+
+//setup strategy - what to do when token returned (what to do if has id, if id is good, or if no or bad token returned)
+let strategy = new JWTStrategy(jwtOptions, (jwt_payload, done) => {
+    User.findById(jwt_payload.id)       //if user found in payload recieved from client, then return done, with no errors
+    .then(user => {
+        return done(null, user);        //then return done with no errors, and a user
+    })
+    .catch(err => {
+        return done(err, false);     //else if error returned, return an error & no user
+    });
+});
+
+passport.use(strategy);     //informing passport that we can also use this strategy
+
 // Use Routes
 app.use('/', indexRouter);
 app.use('/', movieRouter);
 app.use('/', authRouter);
+
+//use API routes
+app.use('/api/auth', authAPIRouter);
 
 
 export default app;
